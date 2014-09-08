62c782b5ac46192d46cdacc479f937e7ca08a637
Jimmy <jimwong1023@gmail.com>
2014-09-08 14:54:32 -0700

    session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+  resources :sessions, only: [:new, :create, :destroy]
+  
   root to: "static_pages#home"
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
